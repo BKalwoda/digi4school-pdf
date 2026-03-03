4cd7b0888340c49fc8b306f5b705465ccf6fa14e
BKalwoda <62034448+BKalwoda@users.noreply.github.com>
2026-03-04 00:30:05 +0100

    Refactor checkVersion to use Axios for requests

--- src/D4SVersionChecker.ts
+++ src/D4SVersionChecker.ts
@@ -1,27 +1,22 @@
 import { JSDOM } from "jsdom";
-import request from "request";
+import Axios from "axios";
 
 export class D4SVersionChecker {
-  public static checkVersion(bookUrl: string, cookies: string, callback: Function) {
-    request(
-      {
-        url: bookUrl + "1/1.svg",
-        method: "GET",
-        headers: {
-          Cookie: cookies,
-        },
-      },
-      async (err, res) => {
-        if (err) return callback(null);
-        const html = new JSDOM(res.body);
-
-        const svgTags = html.window.document.getElementsByTagName("svg");
-        if (svgTags.length >= 1) {
-          return callback(false);
-        } else {
-          return callback(true);
-        }
+  public static async checkVersion(bookUrl: string, cookies: string, callback: Function) {
+    try {
+      const res = await Axios.get(bookUrl + "1/1.svg", {
+        headers: { Cookie: cookies },
+        validateStatus: () => true, // Don't throw on 4xx/5xx
+      });
+      const html = new JSDOM(res.data);
+      const svgTags = html.window.document.getElementsByTagName("svg");
+      if (svgTags.length >= 1) {
+        return callback(false);
+      } else {
+        return callback(true);
       }
-    );
+    } catch {
+      return callback(true);
+    }
   }
 }
